b0ef77e91d33341721d486105ac1c4e4881e586c
ltha5 <haletien68@gmail.com>
2022-10-04 14:18:11 +0700

    Remove JDBC process

--- BE/trans/src/main/java/com/cmc/trans/controller/EmployeeController.java
+++ BE/trans/src/main/java/com/cmc/trans/controller/EmployeeController.java
@@ -3,8 +3,6 @@ package com.cmc.trans.controller;
 import java.sql.SQLException;
 import java.util.List;
 
-import com.cmc.trans.service.EmployeeReadNativeService;
-import com.cmc.trans.service.EmployeeWriteNativeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,35 +19,27 @@ public class EmployeeController {
 	private EmployeeReadService readService;
 	@Autowired
 	private EmployeeWriteService writeService;
-	@Autowired
-	private EmployeeReadNativeService readNativeService;
-	@Autowired
-	private EmployeeWriteNativeService writeNativeService;
 
 	@GetMapping(value = "/employees")
 	public ResponseEntity<?> getEmployees(){
 		List<Employee> employees = readService.selectEmployees();
-		//List<Employee> employees = readNativeService.selectEmployees();
 		return ResponseEntity.ok(employees);
 	}
 
 	@PostMapping(value = "/employee")
 	public ResponseEntity<?> saveEmployee(@RequestBody Employee emp) throws SQLException {
 		Boolean saveStatus = writeService.saveEmployee(emp);
-		//String saveStatus = writeNativeService.saveEmployee(emp);
 		return ResponseEntity.ok(saveStatus);
 	}
 
 	@PutMapping(value = "/employee/{id}")
 	public ResponseEntity<?> updateEmployee(@PathVariable(value = "id") String id, @RequestBody Employee emp) throws SQLException {
 		Employee afterUpdateInfo = writeService.updateEmployee(id, emp);
-		//String afterUpdateInfo = writeNativeService.updateEmployee(emp,id);
 		return ResponseEntity.ok(afterUpdateInfo);
 	}
 
 	@DeleteMapping(value = "/employee/{id}")
 	public ResponseEntity<?> deleteEmployee(@PathVariable(value = "id") String id) throws SQLException {
 		return ResponseEntity.ok(writeService.deleteEmployee(id));
-		//return ResponseEntity.ok(writeNativeService.deleteEmployee(id));
 	}
 }
